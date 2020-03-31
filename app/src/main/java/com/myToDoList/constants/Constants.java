daa4af175abdd856545fa0330bf0dcb7e1d5b0d0
Tindi <ikami@tutanota.com>
2020-03-31 15:22:04 +0300

    Add fingerprint function

--- app/src/main/java/com/myToDoList/constants/Constants.java
+++ app/src/main/java/com/myToDoList/constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final int DATABASE_VERSION = 1;
 
     // Database Name
-    public static final String DATABASE_NAME = "chatapp_db";
+    public static final String DATABASE_NAME = "taskapp_db";
 
 
 
