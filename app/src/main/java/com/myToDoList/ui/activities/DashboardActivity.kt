a22f79966fcbb49c6eaeaa3630d777492c8db234
Tindi <ikami@tutanota.com>
2020-06-27 16:23:23 +0300

    Add fingerprint lock UI

--- app/src/main/java/com/myToDoList/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/myToDoList/ui/activities/DashboardActivity.kt
@@ -18,9 +18,18 @@ import kotlinx.android.synthetic.main.activity_dashboard.*
 import java.util.ArrayList
 import android.graphics.BitmapFactory
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.util.Base64
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
 import com.infideap.drawerbehavior.Advance3DDrawerLayout
+import com.myToDoList.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.*
+import kotlinx.android.synthetic.main.nav_header.view.*
 
 class DashboardActivity : AppCompatActivity() {
     private var list = ArrayList<Task>()
@@ -30,8 +39,10 @@ class DashboardActivity : AppCompatActivity() {
     private var profileName: String? = null
     private var firstrun: Boolean? = true
     private var adapter: TaskAdapter? = null
-    private var navigationView: NavigationView? = null
-    private var drawer: Advance3DDrawerLayout? = null
+   // private var navigationView: NavigationView? = null
+   // private var drawer_layout: Advance3DDrawerLayout? = null
+    private var navHeader: View? = null
+    private var drawerToggle :ActionBarDrawerToggle ? =null
     private var sharedPreferences: SharedPreferences? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,6 +81,7 @@ class DashboardActivity : AppCompatActivity() {
                 if (it == null)
                     return
                 userProfPic.setImageBitmap(decodeBase64(it))
+               // drawer_profile_image.setImageBitmap(decodeBase64(it))
             }
 
 
@@ -105,7 +117,55 @@ class DashboardActivity : AppCompatActivity() {
             adapter?.setData(list)
             adapter?.notifyDataSetChanged()
 
+            navHeader = nav_view?.getHeaderView(0)
+            drawerToggle = ActionBarDrawerToggle(
+                this,
+                drawer_layout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+            )
+            drawerToggle?.getDrawerArrowDrawable()?.setColor(Color.parseColor("#D402F8"))
 
+          //  drawerToggle.setHomeAsUpIndicator(R.drawable.ic_baseline_camera_alt_24)
+
+
+            drawer_layout?.addDrawerListener(drawerToggle!!)
+            drawerToggle?.syncState()
+            drawer_layout?.useCustomBehavior(GravityCompat.START) //assign custom behavior for "Left" drawer
+            drawer_layout?.useCustomBehavior(GravityCompat.END)
+            drawer_layout?.setViewRotation(GravityCompat.START, 15f)
+            drawer_layout?.setViewScale(
+                GravityCompat.START,
+                0.9f
+            ) //set height scale for main view (0f to 1f)
+            drawer_layout?.setViewElevation(
+                GravityCompat.START,
+                20f
+            ) //set main view elevation when drawer open (dimension)
+            drawer_layout?.setViewScrimColor(
+                GravityCompat.START,
+                Color.TRANSPARENT
+            ) //set drawer overlay coloe (color)
+            drawer_layout?.setDrawerElevation(GravityCompat.START, 20f) //set drawer elevation (dimension)
+            drawer_layout?.setContrastThreshold(3f) //set maximum of contrast ratio between white text and background color.
+            drawer_layout?.setRadius(
+                GravityCompat.START,
+                25f
+            ) //set end container's corner radius (dimension)
+
+            var textProfName = navHeader?.findViewById(R.id.drawer_profile_Name) as TextView
+            var imgProfImage = navHeader?.findViewById(R.id.drawer_profile_image) as ImageView
+
+            profileName?.let {
+                textProfName.setText(it)
+            }
+
+            profilePic?.let {
+                if (it == null)
+                    return
+                imgProfImage.setImageBitmap(decodeBase64(it))
+            }
 
         }
     }
