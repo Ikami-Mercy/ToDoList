00255bbcede2e447b28270d0984ab59a19be3a0d
Tindi <ikami@tutanota.com>
2020-06-27 18:11:00 +0300

    Add feedback function

--- app/src/main/java/com/myToDoList/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/myToDoList/ui/activities/DashboardActivity.kt
@@ -19,10 +19,12 @@ import java.util.ArrayList
 import android.graphics.BitmapFactory
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.net.Uri
 import android.util.Base64
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
@@ -184,8 +186,9 @@ class DashboardActivity : AppCompatActivity() {
                                 intent = Intent(applicationContext, FingerprintLockActivity::class.java)
                                 startActivity(intent)
                             }
-
-
+                            R.id.nav_feedback -> {
+                                sendEmail("devtindi@gmail.com", "Notepad Pro Feedback", "")
+                            }
 
 
                             else -> return true
@@ -221,7 +224,34 @@ class DashboardActivity : AppCompatActivity() {
         super.onBackPressed()
     }
 
+    private fun sendEmail(recipient: String, subject: String, message: String) {
+        /*ACTION_SEND action to launch an email client installed on your Android device.*/
+        val mIntent = Intent(Intent.ACTION_SEND)
+        /*To send an email you need to specify mailto: as URI using setData() method
+        and data type will be to text/plain using setType() method*/
+        mIntent.data = Uri.parse("mailto:")
+        mIntent.type = "text/plain"
+        // put recipient email in intent
+        /* recipient is put as array because you may wanna send email to multiple emails
+           so enter comma(,) separated emails, it will be stored in array*/
+        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        //put the Subject in the intent
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        //put the message in the intent
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
 
+
+        try {
+            //start email intent
+           startActivity(Intent.createChooser(mIntent, "Choose Email Client..."))
+        }
+        catch (e: Exception){
+            //if any thing goes wrong for example no email client application or any exception
+            //get and show exception message
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+        }
+
+    }
 
 
 }
---
+++ app/src/main/java/com/myToDoList/utils/GeneralUtils.kt
@@ -0,0 +1,10 @@
+package com.myToDoList.utils
+
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+object GeneralUtils {
+
+
+}
